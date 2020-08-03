b661cba81e82053496c54dda03d1c08f23a3bda9
Avi <avichhetri8@gmail.com>
2020-08-03 20:42:10 +0545

    UseState

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { Component, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Person from './component/Person'
 
-function App() {
-  return (
-    <div className="App">
-      <Person name="Avishek"/>
-    </div>
-  );
+const App = () =>{
+  const [personState , setPersonState] = useState({
+    persons : [
+      {'name':'a'}
+    ]
+  })
+
+  
+const switchNameHandler = (props) => {
+  console.log("clicked")
+  setPersonState({persons:[{'name' :'Clicked'}]}) 
+}
+
+   return(
+        <div className="App">
+            <h1>hello</h1>
+            <button  onClick={switchNameHandler}  >Click Me</button>
+            <Person name={personState.persons[0].name} />
+        </div>
+    
+    );
+
 }
 
 export default App;
+
